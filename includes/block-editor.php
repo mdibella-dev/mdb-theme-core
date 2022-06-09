d834bf31c4ce1cdde1d5a7da0324b8f926b69b3b
mdibella-dev <mdb@marcodibella.de>
2022-06-09 13:15:38 +0200

    Add block-editor-function

---
+++ includes/block-editor.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Settings and functions related to the block editor (aka Gutenberg).
+ *
+ * @author  Marco Di Bella <mdb@marcodibella.de>
+ * @package mdb-theme-core
+ */
+
+
+defined( 'ABSPATH' ) or exit;
+
+
+
+/**
+ * Disables the block editor for various post types.
+ *
+ * @since  1.0.0
+ * @param  bool   $can_edit     The current determination of whether Gutenberg can edit the specified post type (true) or not (false).
+ * @param  string $post_type    The post type.
+ * @return bool                 The new definition of whether Gutenberg can edit the specified post type (true) or not (false).
+ *
+ * @see    https://digwp.com/2018/04/how-to-disable-gutenberg/
+ * @see    https://stackoverflow.com/questions/52199629/how-to-disable-gutenberg-editor-for-certain-post-types/52199630
+ * @see    https://www.billerickson.net/disabling-gutenberg-certain-templates/
+ */
+
+function mdb_tc__disable_gutenberg( $can_edit, $post_type )
+{
+    if( ( 'vortrag' === $post_type ) or ( 'publikation' === $post_type ) ) :
+        $can_edit = false;
+    endif;
+
+    return $can_edit;
+}
+
+add_filter( 'gutenberg_can_edit_post_type', 'mdb_tc__disable_gutenberg' );
+add_filter( 'use_block_editor_for_post_type', 'mdb_tc__disable_gutenberg', 10, 2 );
--- plugin.php
+++ plugin.php
@@ -28,4 +28,5 @@ require_once( $plugin_path . 'includes/post-types/post-type-publikation.php' );
 require_once( $plugin_path . 'includes/post-types/post-type-vortrag.php' );
 require_once( $plugin_path . 'includes/api/publikation.php' );
 require_once( $plugin_path . 'includes/acf.php' );
+require_once( $plugin_path . 'includes/block-editor.php' );
 require_once( $plugin_path . 'includes/setup.php' );
